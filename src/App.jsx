03c0bfb9b5f4a077fa6853557e4a00b9f978d1a8
brhnam ghasemi <behnam.ghasemi082@gmail.com>
2024-02-19 09:14:40 +0330

    add Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,1 @@
-export default function App() {
-  return <div className="bg-red-400 h-20">111</div>;
-}
+export default function App() {}
---
+++ src/Pages/Form/input.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
+
+export default function Input(props) {
+  const inputRef = useRef(null);
+  const { label, icon, type, err, ...other } = props;
+  function focusInput() {
+    inputRef.current.focus();
+  }
+  return (
+    <div
+      className="flex flex-col gap-1 bg-white h-24 w-3/4 rounded-xl px-6 py-3 text-[#8f8f8f] shadow-md"
+      onClick={focusInput}
+    >
+      <label htmlFor="">{label}</label>
+      <div className="flex gap-1 flex-grow items-center w-full">
+        <FontAwesomeIcon icon={icon} />
+        <input
+          {...other}
+          type={type}
+          ref={inputRef}
+          className="h-full w-full text-center focus:outline-none border-b-2 focus:text-black"
+        />
+      </div>
+      <p className="text-xs text-red-500">{err}</p>
+    </div>
+  );
+}
