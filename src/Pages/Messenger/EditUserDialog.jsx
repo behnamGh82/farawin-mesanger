89e5e39c23a31183e256a8f0f7445d64ce4f8234
devlopermh <112709489+mamaddep8421@users.noreply.github.com>
2024-02-29 21:06:47 +0330

    EditContactMEssenger,codedbasic react

--- src/Pages/Messenger/EditUserDialog.jsx
+++ src/Pages/Messenger/EditUserDialog.jsx
@@ -14,7 +14,47 @@ export default function EditUserDialog(props) {
   //   رجکس موبایل برای اعتبار سنجی شماره تماس
   const phoneRegex =
     /^(?:(?:(?:\\+?|00)(98))|(0))?((?:90|91|92|93|99)[0-9]{8})$/;
-  //
+  // تابع اعتبار سنجی برای نام و نام خانوادگی مخاطب
+  const validateName = (Editname) => {
+    if (Editname == "") {
+      return "Epmty";
+    } else if (Editname.length < 10) {
+      return "notValid";
+    }
+    return "true";
+  };
+  // تابع اعتبار سنجی برای شماره تماس مخاطب
+  const validatePhone = (Editphone) => {
+    if (Editphone == "") {
+      return "Epmty";
+    } else if (!phoneRegex.test(Editphone)) {
+      return "notValid";
+    }
+    return "true";
+  };
+  // ذخیره مقدار ویرایش شده جهت مقایسه و نمایش دادن پیام درست به کاربر
+  const EditNameErorr = EditName;
+  const EditPhoneErorr = EditPhone;
+  //ذخیره پیغام برای نمایش  به کاربر
+  const [Erorr, SetErorr] = useState("");
+  // درخواست زدن به apiبرای ویراریش مخاطب
+  // این درخواست فانکشن داخل باتن قرار میگیرد برای ارسال درخواست و ویرایش
+  const handleButton = async () => {
+    const token = localStorage.getItem("token");
+    const addContact = await fetch(
+      "https://farawin.iran.liara.run/api/contact",
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          authorization: token,
+        },
+        body: JSON.stringify({ username: EditPhone, name: EditName }),
+      }
+    );
+    const res = await addContact.json();
+    console.log(res);
+  };
   return (
     <div>
       <p>test</p>
